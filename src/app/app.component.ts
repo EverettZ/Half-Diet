17ca24ead2f6b44b2e8ee4d2b35f70c4475eaa81
Everett Zettersten <paul.zettersten@emoney.com>
2018-09-07 22:53:28 -0400

    Initial home page content setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,23 +4,34 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
-@Component({
+@Component( {
   selector: 'app-root',
   templateUrl: 'app.component.html'
-})
+} )
 export class AppComponent {
-  constructor(
+
+  constructor (
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar
   ) {
+
     this.initializeApp();
+
   }
 
   initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
+
+    this.platform
+      .ready()
+      .then( () => {
+
+        this.statusBar
+          .styleDefault();
+
+        this.splashScreen
+          .hide();
+
+      } );
   }
 }
